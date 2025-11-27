08c4719db8c875b50ff690ab6a156d5b439dd30e
Francisco M.P. <9068592+fmpanggabean@users.noreply.github.com>
2025-11-27 16:02:52 +0700

    add cinemachine and camera controller

---
+++ Assets/Apps/Scripts/Cameras/CameraController.cs
@@ -0,0 +1,33 @@
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController Instance;
+
+    private CinemachineCamera cinemachineCamera;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        cinemachineCamera = GetComponentInChildren<CinemachineCamera>();
+    }
+
+    void Start()
+    {
+        SetFocusTo(FindFirstObjectByType<Player>().transform);
+    }
+
+    public void SetFocusTo(Transform target)
+    {
+        cinemachineCamera.Follow = target;
+    }
+}
