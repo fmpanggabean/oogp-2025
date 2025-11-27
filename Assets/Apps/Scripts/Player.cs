603825f9850f958c5f9927797d48e54efc9f07d9
Francisco M.P. <9068592+fmpanggabean@users.noreply.github.com>
2025-11-27 15:29:53 +0700

    player input

---
+++ Assets/Apps/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInput))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody2D Rigidbody2D;
+    private PlayerInput PlayerInput;
+
+    void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        PlayerInput = GetComponent<PlayerInput>();
+    }
+
+    void OnEnable()
+    {
+        PlayerInput.actions["Move"].performed += SetDirection;
+    }
+
+    void OnDisable()
+    {
+        PlayerInput.actions["Move"].performed -= SetDirection;
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        Rigidbody2D.linearVelocity = direction * speed;
+    }
+
+    public void SetDirection(CallbackContext ctx)
+    {
+        if (ctx.performed || ctx.canceled)
+        {
+            SetDirection(ctx.ReadValue<Vector2>());
+        }
+    }
+}
